830def29e72edb3f7082cf504dbad451f02cea89
Salil Kr Singh <42035921+salil498@users.noreply.github.com>
2020-08-05 16:37:06 +0530

    Update Power-of-four.java

--- Power-of-four.java
+++ Power-of-four.java
@@ -12,3 +12,11 @@ class Solution {
         
     }
 }
+
+
+////////////////without loop 1 line solution/////////////
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        return (num > 0) && ((num & (num - 1)) == 0) && ((num & 0x55555555) == num);
+    }
+}
