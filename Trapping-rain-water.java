447c49a8e9cba03edb6c322b96d5a09d0c0a96ed
Salil Kr Singh <42035921+salil498@users.noreply.github.com>
2020-08-05 21:49:39 +0530

    Update Trapping-rain-water.java

--- Trapping-rain-water.java
+++ Trapping-rain-water.java
@@ -21,3 +21,26 @@ class Solution {
         return sum;
     }
 }
+
+//////////////pre computing lmax and max...O(N) time and O(N) space solution/////////////
+
+class Solution {
+    public int trap(int[] height) {
+        int sum=0;
+        if(height.length==0)
+            return 0;
+        int[] lmax=new int[height.length];
+        int[] rmax=new int[height.length];
+        lmax[0]=height[0];
+        rmax[height.length-1]=height[height.length-1];
+        for(int i=1;i<height.length;i++)
+            lmax[i]=Math.max(lmax[i-1],height[i]);
+        for(int i=height.length-2;i>=0;i--)
+            rmax[i]=Math.max(rmax[i+1],height[i]);
+        for(int i=0;i<height.length;i++){
+                int total=Math.min(lmax[i],rmax[i]);
+                sum += total-height[i];    
+        }
+        return sum;
+    }
+}
