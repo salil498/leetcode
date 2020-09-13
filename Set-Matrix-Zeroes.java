7d204b61399c3b9e520090c89e893ea59b78d3f3
Salil Kr Singh <42035921+salil498@users.noreply.github.com>
2020-09-13 15:40:16 +0530

    Create Set-Matrix-Zeroes.java

---
+++ Set-Matrix-Zeroes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int[] row=new int[matrix.length];
+        int[] col=new int[matrix[0].length];
+        Arrays.fill(row, 0, matrix.length, 1); 
+        Arrays.fill(col, 0, matrix[0].length, 1); 
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    row[i]=0;
+                    col[j]=0;
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(row[i]==0 || col[j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        
+        
+    }
+}
